0687ffb9c14cbedb236d00797268577dc3b9ce7e
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-13 05:15:52 +0100

    Reset and Update Password Endpoint

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const controller = require("../controller/userController")
+const controller = require("../controller/userController");
+const { verifyToken } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", controller.registerUser);
 router.post("/login", controller.loginUser);
+router.post("/forgotPassword", controller.forgotPassword);
+router.patch("/resetPassword", controller.resetPassword);
+router.patch("/changePassword", verifyToken, controller.changePassword);
 
 module.exports = router;
