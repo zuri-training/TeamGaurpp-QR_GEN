57988348ae3ea7f71afa5bc02caf34ec6d0fad67
Abiodun Shittu <jerrywizklay1@gmail.com>
2022-12-14 11:20:33 +0100

    Get QR Codes Endpoint

--- routes/qrRoutes.js
+++ routes/qrRoutes.js
@@ -4,5 +4,8 @@ const { verifyToken } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/", verifyToken, controller.generateCode);
+router.get("/", verifyToken, controller.getAllQrCodes);
+router.get ("/:id", verifyToken, controller.getAQrCode)
+router.delete("/:id", verifyToken, controller.deleteQrCode)
 
 module.exports = router;
