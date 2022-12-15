8ee4b69da8779c5bd922f9b1ed266ad440b45103
Increase <increasechris124@gmail.com>
2022-12-15 15:22:10 +0100

    feat: updated styles for dashboard 2 and 3

--- frontend/dashboard1/dashboard1.js
+++ frontend/dashboard1/dashboard1.js
@@ -3,43 +3,39 @@ const user = JSON.parse(localStorage.getItem("user"));
 const token = localStorage.getItem("token");
 const qrCodesContainer = document.querySelector("#qrcodes_container");
 
-
-
 window.onload = () => {
-    let qrCodes  = ""
-    userEmail.innerHTML = user.email;
-    const url = "http://qr-gen.eu-4.evennode.com/api/qr_gen/";
-    
-    fetch(url, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data.allQrCodes)
-        if(data.allQrCodes.length > 0) {
-            for (let qrCode of data.allQrCodes) {
-                qrCodes += `
+  let qrCodes = "";
+  userEmail.innerHTML = user.email;
+  const url = "http://qr-gen.eu-4.evennode.com/api/qr_gen/";
+
+  fetch(url, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${token}` },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.allQrCodes.length > 0) {
+        for (let qrCode of data.allQrCodes) {
+          qrCodes += `
                 <div class="qrcode" id=${qrCode._id}>
                     <img src=${qrCode.qrCode} alt="qr code" width = "150px" />
                     <p>${qrCode.title ? qrCode.title : "Untitled"}</p>
-                    <p>${Date((qrCode.createdAt)).slice(0,15)}</p>
+                    <p>${Date(qrCode.createdAt).slice(0, 15)}</p>
                 </div>
-            ` 
-            }
+            `;
         }
-        qrCodesContainer.innerHTML = qrCodes;
-
+      }
+      qrCodesContainer.innerHTML = qrCodes;
     })
-      .catch((error) => console.log(error));
+    .finally(() => {
+      const qrCodesDivs = document.querySelectorAll(".qrcode");
 
-  const qrCodesDivs = document.querySelectorAll('.qrcode');
-
-  qrCodesDivs.forEach((qrCodeDiv) => {
-    qrCodeDiv.addEventListener('click', () => {
-        const qrCodeId = qrCodeDiv.getAttribute("id");
-        console.log(qrCodeId)
+      qrCodesDivs.forEach((qrCodeDiv) => {
+        qrCodeDiv.addEventListener("click", () => {
+          const qrCodeId = qrCodeDiv.getAttribute("id");
+          console.log(qrCodeId);
+        });
+      });
     })
-  })
-}
-
+    .catch((error) => console.error(error));
+};
